a18e92fb0f575ab3a047e27eda426639b91664fd
Pumipat <88040178+o0SoloWolf0o@users.noreply.github.com>
2023-04-02 22:47:43 +0700

    master

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM openjdk:19
 VOLUME /tmp
 ARG JAR_FILE
+WORKDIR /app
 COPY target/miniproject-0.0.1-SNAPSHOT.jar /app/miniproject.jar
 ENTRYPOINT ["java","-jar","/app/miniproject.jar"]
--- src/test/java/com/miniproject/miniproject/RoleRepositoryTests.java
+++ src/test/java/com/miniproject/miniproject/RoleRepositoryTests.java
@@ -1,33 +1,33 @@
-// package com.miniproject.miniproject;
+package com.miniproject.miniproject;
 
-// import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-// import java.util.List;
+import java.util.List;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-// import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
-// import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-// import org.springframework.test.annotation.Rollback;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
 
-// @DataJpaTest
-// @AutoConfigureTestDatabase(replace = Replace.NONE)
-// @Rollback(false)
-// public class RoleRepositoryTests {
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class RoleRepositoryTests {
 
-// 	@Autowired private RoleRepository repo;
+	@Autowired private RoleRepository repo;
 	
-// 	@Test
-// 	public void testCreateRoles() {
-// 		Role user = new Role("User");
-// 		Role admin = new Role("Admin");
+	@Test
+	public void testCreateRoles() {
+		Role user = new Role("User");
+		Role admin = new Role("Admin");
 
-// 		repo.saveAll(List.of(user, admin));
+		repo.saveAll(List.of(user, admin));
 		
-// 		List<Role> listRoles = repo.findAll();
+		List<Role> listRoles = repo.findAll();
 		
-// 		assertThat(listRoles.size()).isEqualTo(2);
-// 	}
+		assertThat(listRoles.size()).isEqualTo(8);
+	}
 	
-// }
+}
